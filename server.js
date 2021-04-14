9cc38cac21c8573489f821d8fe3036788805d890
sharath <Bharath.s@goodworklabs.com>
2021-04-14 14:31:05 +0530

    update port for heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
 
-app.get( function (req, res){
-	res.send('todo API Root');
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
 });
 
-app.listen(PORT , function(){
-	console.log('express the listening on port  '+ PORT +'!');
+app.listen(PORT, function () {
+	console.log('Express listening on port ' + PORT + '!');
 });
