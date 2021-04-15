6650d22e376cd5b99461dfa8794a544683471b50
sharath <Bharath.s@goodworklabs.com>
2021-04-15 15:52:38 +0530

    delete the todos

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -19,13 +20,7 @@ app.get('/todos', function(req, res){
 
 app.get('/todos/:id', function(req,res){
 	var todoId = parseInt(req.params.id);
-	var matchedTodo;
-
-	todos.forEach(function(todo){
-		if(todoId ===todo.id){
-			matchedTodo=todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if(matchedTodo){
 		res.json(matchedTodo);
@@ -35,9 +30,14 @@ app.get('/todos/:id', function(req,res){
 });
 
 app.post('/todos', function(req,res){
-	var body= req.body;
+	var body= _.pick( req.body , 'description','completed');;
 
-	//add id body
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return(res.status(400).send());
+	}
+
+	body.description = body.description.trim();
 	body.id= todosnextId++;
 
 	//push body
@@ -47,6 +47,19 @@ app.post('/todos', function(req,res){
 	res.json(body);	
 
 });
+
+app.delete('/todos/:id', function(req,res){
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo){
+		res.status(404).json({"error": "no todo found with that matching id"});
+		}else{
+			todos=_.without(todos, matchedTodo);
+			res.json(matchedTodo);
+		}
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
