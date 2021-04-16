c17baed86047faf89013a877a18216eaa9e1d9e6
sharath <Bharath.s@goodworklabs.com>
2021-04-16 13:23:05 +0530

    checking completed is boolean

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/todos', function(req, res){
-	res.json(todos);
+	var queryparams = req.query;
+	var filtertodos = todos;
+
+	if(queryparams.hasOwnProperty('completed') && queryparams.completed === 'true'){
+		filtertodos = _.where(filtertodos, {completed: true});
+	}else if(queryparams.hasOwnProperty('completed') && queryparams.completed === 'false'){
+		filtertodos = _.where(filtertodos, {completed: false});
+	}
+	res.json(filtertodos);
 });
 
 app.get('/todos/:id', function(req,res){
@@ -30,7 +38,7 @@ app.get('/todos/:id', function(req,res){
 });
 
 app.post('/todos', function(req,res){
-	var body= _.pick( req.body , 'description','completed');;
+	var body= _.pick( req.body , 'description','completed')
 
 
 	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
@@ -59,6 +67,33 @@ app.delete('/todos/:id', function(req,res){
 			res.json(matchedTodo);
 		}
 });
+
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
+
 
 
 app.listen(PORT, function () {
